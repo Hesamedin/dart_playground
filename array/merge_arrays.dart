1c087955db7419a662738ef716a94844182f81de
Hesam Kamalan <hesamedin.kamalan@atco.com>
2022-04-22 16:43:22 -0600

    Merge two sorted array function added.

--- array/merge_arrays.dart
+++ array/merge_arrays.dart
@@ -1,21 +1,46 @@
 main() {
   List<int> nums1 = [1, 2, 3, 0, 0, 0];
   List<int> nums2 = [2, 5, 6];
-  List<int> answer = mergeArrays(nums1, 3, nums2, 3);
+  List<int> answer = mergeArrays_2(nums1, 3, nums2, 3);
   print('Answer: $answer');
 
   nums1 = <int>[1];
   nums2 = <int>[];
-  answer = mergeArrays(nums1, 1, nums2, 0);
+  answer = mergeArrays_2(nums1, 1, nums2, 0);
   print('Answer: $answer');
 
   nums1 = <int>[0];
   nums2 = <int>[1];
-  answer = mergeArrays(nums1, 0, nums2, 1);
+  answer = mergeArrays_2(nums1, 0, nums2, 1);
   print('Answer: $answer');
 }
 
-List<int> mergeArrays(List<int> nums1, int m, List<int> nums2, int n) {
+/// Method 2
+List<int> mergeArrays_2(List<int> nums1, int m, List<int> nums2, int n) {
+  int p = nums1.length - 1;
+  int i = m - 1;
+  int j = n - 1;
+
+  if(i < 0) return nums2;
+
+  while(i >= 0 && j >= 0) {
+    if(nums2[j] > nums1[i]) {
+      nums1[p] = nums2[j];
+      j--;
+      p--;
+    } else {
+      nums1[p] = nums1[i];
+      nums1[i] = 0;
+      i--;
+      p--;
+    }
+  }
+
+  return nums1;
+}
+
+/// Method 1
+List<int> mergeArrays_1(List<int> nums1, int m, List<int> nums2, int n) {
   List<int> result = [];
   int i = 0;
   int j = 0;
